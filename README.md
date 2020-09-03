438b8f77a33295239fc32726f24fcb96b9d2eb9e
dawidK5 <19233116@studentmail.ul.ie>
2020-09-03 19:09:19 +0100

    Clean up the repository

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# JavaProject2
-Playlists and tracks management
+# Playlist and track management - Java Group Project
+This is our group project for CS4222 module. The purpose of this Java code is to allow to managing of audio tracks and to create music playlists.
--- Track.java
+++ Track.java
@@ -4,66 +4,78 @@
  * @author
  * Dawid Kocik          19233116
  * Tommy Crowley        19263546
- * Elliot Cleary        19264259
  * Ronan McMorrow       19235208        
  * Oscar Bogenberger    19275153
  * 
  * @version 16/04/2020
  */
 import java.util.*;
-// Look through the ContactList (on GitHub in a subfolder), some of the code needed is there
-// Add private helper methods if needed
-public class Track {
-    private String title;
+public class Track implements Comparable<Track> {
     private String artist;
-    private int year;
+    private String title;
     private int duration;
-    
-    // Track class should implement Comparable and include a compareTo method that determines 
-    // the natural ordering of Track instances. Tracks should be ordered by title (case 
-    // insensitive)
-    
-    // 2 constructors
+    private int year;
+    int minutes;
+    int seconds;
     public Track(String title, String artist) {
-        // 1st constructor, initialise instance variables e.g. this.title = title;
-        // and set year and duration to 0
-        
+        this.artist = artist;
+        this.title = title;
+        duration = 0;
+        year = 0;
     }
+
     public Track(String title, String artist, int year, int duration) {
-        // 2nd constructor, all 4 pieces of data
+        this.artist = artist;
+        this.title = title;
+        this.duration = duration;
+        this.year = year;
     }
-    public String toString() {
-        // displays all track details in a neat way
-        // track duration should be given in MM:SS format
-        return title;
+
+    public void setName(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return this.artist;
+    }
+    public void setArtist(String Artist) {
+        this.artist = Artist;
     }
     public void setTitle(String title) {
-        // This allows the title to be changed
+        this.title = title;
     }
-    public String getTitle() { 
-        // This returns the current title
-        return title;
-    }
-    public void setArtist(String artist) {
-        // This allows the artist name to be changed
-    }
-    public String getArtist() {
-        // This returns the current artist name
-        return artist;
-    }
-    public void setYear(int year) {
-        // This allows the year of release to be changed
-    }
-    public int getYear() {
-        // This returns the current year of release
-        return year;
-    }
-    public void setDuration(int seconds) {
-        // This allows the track duration to be changed
-    }
-    public int getDuration() {
-        // This returns the current duration
-        return duration;
+    public String getTitle() {
+        return this.title ;
     }
     
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+    public int getDuration(){
+        return this.duration;
+    }
+    
+     public void setYear(int year) {
+        this.year = year;
+    }
+    
+    public int getYear(){
+        return this.year;
+    }
+    
+    public String toString() {
+        int sec;
+        minutes = this.duration / 60;
+        seconds = this.duration % 60;
+        sec = seconds%10;
+        seconds = seconds/10;
+        return "(" + this.title + " _ " + this.artist + " _ " + this.year + " _ " + minutes + ":" + (seconds) + sec  + ")";
+    }
+
+    public int compareTo(Track other) {
+        // NOTE: the comparison is CASE INSENSITIVE
+        return this.title.compareToIgnoreCase(other.title);
+    }
 }
+
